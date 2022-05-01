850fa270b6cf7297266d169fd733652121f9a9a0
Devyaanop21 <95429714+Devyaanop21@users.noreply.github.com>
2022-05-01 13:34:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+console.log("Hello User")
+console.log("Im a Bot and my name is hachi")
+
+
+
+
+
+
+console.log("and my developers name is :-")
+
+var first_name = "Devyaan";
+var last_name = "jasra";
+var full_name = first_name.concat(" ",last_name)
+ console.log(full_name)
+
+console.log("i can do mathamatics as well;-")
+
+var a = 10;
+var b = 5;
+ var add = a + b ;
+ console.log(add)
+
